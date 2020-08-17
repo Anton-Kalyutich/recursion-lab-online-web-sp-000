17539cd5557887f4e7ce25772d3378ab41d2ff2e
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-17 03:59:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// Code your solution here!
+function printString(string) {
+  console.log(string[0]);
+  
+  if (string.length > 1) {
+     let substring = string.substring(1, string.length);
+     printString(substring);
+  } else {
+    return true;
+  }
+}
 
+function reverseString(string) {
+  if (string.length > 1) {
+    string = reverseString(string.slice(1)) + string.charAt(0);
+  } else {
+    return string;
+  }
+  return string;
+}
+
+function isPalindrome (string) {
+  
+}
+
+function addUpTo(array, index) {
+  
+  }
+}
+
+function includesNumber() {
+  
+}
