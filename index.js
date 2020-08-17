325b8a17de569b8bcb70aa14be10d47428ba618d
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-17 04:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function isPalindrome (string) {
 }
 
 function addUpTo(array, index) {
+  let sum = 0;
   if (array.length > 1) {
     let sum = array[0] + addUpTo(array.slice(1, index + 1));
   } else {
